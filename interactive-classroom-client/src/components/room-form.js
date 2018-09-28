b23954cd2eb8c4ea2b60b04b5d4f1d48a2b2c2de
nivalen292 <nivalen292@outlook.com>
2018-09-28 22:45:10 +0300

    room-form now redirects to the certain room component

--- interactive-classroom-client/src/components/room-form.js
+++ interactive-classroom-client/src/components/room-form.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { putRequest as put } from '../utils/requests';
+import { withRouter } from 'react-router-dom';
+
+// react notifications
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 class RoomForm extends Component {
     constructor(props) {
@@ -14,7 +19,12 @@ class RoomForm extends Component {
         this.setState({ inputValue: '' });
         put('http://localhost:5000/api/room', { name: name })
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((room) => {
+                this.props.history.push('/room/' + room.roomID);
+            })
+            .catch((error) => {
+                NotificationManager.error('There is no room with that name!', 'Error', 5000);
+            });
     }
 
     updateInputValue(e) {
@@ -33,4 +43,4 @@ class RoomForm extends Component {
     }
 }
 
-export default RoomForm;
+export default withRouter(RoomForm);
