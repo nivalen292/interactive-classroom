b5ba9a7104d78b239d98d920dcb78a9ceeefd607
nivalen292 <nivalen292@outlook.com>
2018-10-14 16:51:24 +0300

    removed constants from heroku

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const async = () => {
     return Promise.resolve();
 };
 
-const { config } = require('./constants');
+//const { config } = require('./constants');
 
 async()
-    .then(() => require('./db').init(process.env.DB_URL || config.connectionString))
+    .then(() => require('./db').init(process.env.DB_URL || require('./constants').connectionString))
     .then((db) => require('./data').init(db))
     .then((data) => require('./app').init(data))
     .then((http) => {
-        http.listen(process.env.PORT || config.port, '0.0.0.0', () => {
+        http.listen(process.env.PORT || require('./constants').port, '0.0.0.0', () => {
             console.log(`Server running at localhost:${config.port}`);
         });
     });
