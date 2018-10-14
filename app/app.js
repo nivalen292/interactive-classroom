e1a787a76ab92b4ea97fab67dcdcdb7a8831c18a
nivalen292 <nivalen292@outlook.com>
2018-10-14 17:02:21 +0300

    removed code for production mode

--- app/app.js
+++ app/app.js
@@ -16,14 +16,14 @@ const init = (data) => {
     app.use(cookieParser());
     app.use(cors());
 
-    if (process.env.NODE_ENV === 'production') {
-        // Serve any static files
-        app.use(express.static(path.join(__dirname, 'interactive-classroom-client/build')));
-        // Handle React routing, return all requests to React app
-        app.get('*', function (req, res) {
-            res.sendFile(path.join(__dirname, 'interactive-classroom-client/build', 'index.html'));
-        });
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //     // Serve any static files
+    //     app.use(express.static(path.join(__dirname, 'interactive-classroom-client/build')));
+    //     // Handle React routing, return all requests to React app
+    //     app.get('*', function (req, res) {
+    //         res.sendFile(path.join(__dirname, 'interactive-classroom-client/build', 'index.html'));
+    //     });
+    // }
 
     app.use('/', express.static('interactive-classroom-client/build'));
 
