a944c9c17e1a0a607c509e51ded66a0a7ef5fc30
nivalen292 <nivalen292@outlook.com>
2018-10-14 18:24:09 +0300

    directly added host address to requesters

--- interactive-classroom-client/src/utils/client-constants.js
+++ interactive-classroom-client/src/utils/client-constants.js
@@ -1,5 +1,6 @@
 const config = {
-    endpoint: `http://localhost:${process.env.REACT_APP_PORT || 5000}`
+    endpoint: 'https://interactiveroom.herokuapp.com'
+    //endpoint: `http://localhost:${process.env.REACT_APP_PORT || 5000}`
 };
 
 export default config;
