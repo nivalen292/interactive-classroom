a9714ad089f04a17cf24d14a459148d18529795e
nivalen292 <nivalen292@outlook.com>
2018-10-14 17:38:30 +0300

    before adding client constants

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const async = () => {
 //const { config } = require('./constants');
 
 async()
-    .then(() => require('./db').init(process.env.DB_URL || require('./constants').connectionString))
+    .then(() => require('./db').init(process.env.DB_URL || require('./constants').config.connectionString))
     .then((db) => require('./data').init(db))
     .then((data) => require('./app').init(data))
     .then((http) => {
-        http.listen(process.env.PORT || require('./constants').port, '0.0.0.0', () => {
-            console.log(`Server running at localhost:${process.env.PORT || require('./constants').port}`);
+        http.listen(process.env.PORT || require('./constants').config.port, '0.0.0.0', () => {
+            console.log(`Server running at localhost:${process.env.PORT || require('./constants').config.port}`);
         });
-    });
+    })
+    .catch((e) => console.log(e.stack));
